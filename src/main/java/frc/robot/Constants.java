c2e40b785d9a6f6cf793a90b33cd76696d14d304
InvisibleTiger <jeevanadhya.vinothbabu@gmail.com>
2023-03-09 07:47:55 -0600

    Work on fixing some build errors.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,7 @@
 
 package frc.robot;
 import edu.wpi.first.wpilibj.SerialPort;
+import edu.wpi.first.math.util.Units;
 
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
@@ -14,28 +15,28 @@ import edu.wpi.first.wpilibj.SerialPort;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    public static final double trackWidth = 1.0;
-    public static final double wheelBase = 1.0;
+    public static final double trackWidth = Units.inchesToMeters(27.00);
+    public static final double wheelBase = Units.inchesToMeters(25.00);
 
     public static final SerialPort.Port navXPort = SerialPort.Port.kMXP;
 
-    public static final int frontLeftDrive = 0;
-    public static final int frontLeftSteer = 0;
-    public static final int frontLeftEncoder = 0;
+    public static final int frontLeftDrive = 40;
+    public static final int frontLeftSteer = 41;
+    public static final int frontLeftEncoder = 3;
     public static final double frontLeftOffset = -Math.toRadians(0.0);
 
-    public static final int frontRightDrive = 0;
-    public static final int frontRightSteer = 0;
-    public static final int frontRightEncoder = 0;
+    public static final int frontRightDrive = 31;
+    public static final int frontRightSteer = 30;
+    public static final int frontRightEncoder = 2;
     public static final double frontRightOffset = -Math.toRadians(0.0);
 
-    public static final int backLeftDrive = 0;
-    public static final int backLeftSteer = 0;
-    public static final int backLeftEncoder = 0;
+    public static final int backLeftDrive = 21;
+    public static final int backLeftSteer = 20;
+    public static final int backLeftEncoder = 1;
     public static final double backLeftOffset = -Math.toRadians(0.0);
 
-    public static final int backRightDrive = 0;
-    public static final int backRightSteer = 0;
+    public static final int backRightDrive = 11;
+    public static final int backRightSteer = 10;
     public static final int backRightEncoder = 0;
     public static final double backRightOffset = -Math.toRadians(0.0);
 }
